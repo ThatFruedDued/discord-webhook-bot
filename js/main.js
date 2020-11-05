c0fa30224150600807c1bf50516071d8a3e921c4
Frued Dued <thatfrueddued@github.com>
2020-11-05 18:12:53 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ function w(query){
 function load(url){
   document.clear();
   loading();
+  if(url.toLowerCase().startsWith("http://") || url.toLowerCase().startsWith("https://"))
+    url = "https://corsbyp.herokuapp.com/" + url;
   fetch(url).then(r => r.text().then(t => {
     document.clear();
     document.write(t);
   }));
 }
 function loading(){
-  document.write("<!DOCTYPE html><html><head><title>Discord Webhook Bot</title></head><body><style>#loading{position:absolute;top:0;bottom:0;left:0;right:0;display:block;margin:auto;}</style><img id='loading' src='assets/loading.gif'></body></html>");
+  document.write("<!DOCTYPE html><html><head><title>Loading...</title></head><body><style>#loading{position:absolute;top:0;bottom:0;left:0;right:0;display:block;margin:auto;}</style><img id='loading' src='assets/loading.gif'></body></html>");
 }
